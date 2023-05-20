e35c8b405157f8983d7d77a621054feaef225e83
Ese <amadasunese@gmail.com>
2023-05-20 13:31:07 -0700

    Fix my code challenge

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ FizzBuzz
 """
+
 import sys
 
 
@@ -17,10 +18,10 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:  # Check multiples of both 3 and 5 first
             tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
--- 2-sort.rb
+++ 2-sort.rb
@@ -6,21 +6,21 @@
 
 result = []
 ARGV.each do |arg|
-    # skip if not integer
+    # skip if not an integer
     next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
+    # convert to an integer
     i_arg = arg.to_i
-    
-    # insert result at the right position
+
+    # insert the result at the right position
     is_inserted = false
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
+        if result[i] <= i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
